1a9e451d46b28b20e8e3a26a24742efcd603a259
Your Name <your.email@example.com>
2025-05-13 11:44:28 +0500

    to add files

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from accounts.manage import UserManager
+from common.models import BaseModel
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    email = models.EmailField(unique=True)
+    passeword = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
